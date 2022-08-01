d52d230ef8de137136ab662cb5ab879cfe3aca49
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-07-31 22:22:41 -0700

    fix

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,9 @@
     <header class="bg-white shadow" v-if="$route.meta.title">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <h1 class="nav text-3xl font-bold leading-tight text-gray-600">
-          <router-link to="/">{{ $route.meta.title }}</router-link> | <router-link to="contact">Contact Me</router-link> |
-          <router-link to="projects">Projects</router-link>
+          <router-link to="/">{{ $route.meta.title }}</router-link> | <router-link to="projects">Projects</router-link> |
+          <router-link to="contact">Contact Me</router-link> 
+
         </h1>
       </div>
     </header>
@@ -32,7 +33,7 @@
   position: fixed;
   z-index: 3;
   top: 0;
-  left: 90px;
+  right: 90px;
 }
 @media screen and (max-width: 768px) {
   .nav {
