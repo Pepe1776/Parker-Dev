6a62258a73cac281ae8ce280e4f8e6558c49d1c2
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-08-01 05:53:17 -0700

    added content

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>
-        Projects
+    <div class="main-cont">
+        <Icon style="font-size: 6rem;" icon="logos:arangodb-icon" />
+        <Icon style="font-size: 6rem;" icon="logos:metamask-icon" />
+        <Icon style="font-size: 6rem;" icon="logos-vue" />
+        <Icon style="font-size: 6rem;" icon="emojione:blond-haired-person-medium-skin-tone" />
+        <Icon style="font-size: 6rem;" icon="noto:cut-of-meat" />
     </div>
 </template>
 <script setup>
-
+import { Icon } from '@iconify/vue';
 </script>
-<style>
+
+<style scoped>
+.main-cont {
+    display: flex;
+    flex-direction: row;
+    gap: 10%;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    margin-top: -15%;
+}
     
 </style>
